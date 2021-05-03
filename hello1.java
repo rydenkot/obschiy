5c139fe15b1a5e5a25ae27d8e0d99d2fb77f129f
rydenkot <45936536+rydenkot@users.noreply.github.com>
2021-05-03 18:20:02 +0300

    Add files via upload

---
+++ hello1.java
@@ -0,0 +1,11 @@
+package com.max.idea;
+
+public class hello {
+            public static double b1, b2=123.25;
+            public static short a1=32767;
+    public static void main(String[] args) {
+        System.out.println(b1);
+        System.out.println(b2);
+        System.out.println(a1);
+    }
+}
