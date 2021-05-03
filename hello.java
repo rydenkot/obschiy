69126798dd76360965bd297a1c09d4136b444f04
rydenkot <45936536+rydenkot@users.noreply.github.com>
2021-05-04 00:17:16 +0300

    Add files via upload

--- hello.java
+++ hello.java
@@ -1,7 +1,17 @@
 package com.max.idea;
 
 public class hello {
-    public static void main(String[] args) {
-        System.out.println("Hello World");
-    }
+    public static void main(String[] args)
+    {
+    int[] a =  {1, 2, 3, 5, 7};
+
+    int temp = a[0];
+        a[0] = a[4];
+        a[4] = temp;
+        System.out.print("Сумма первого и среднего элемента"+ " ");
+        System.out.println(a[0]+a[2]);
+
+
+}
+
 }
