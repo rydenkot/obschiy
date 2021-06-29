435dba6460f1dd315aedb972dbbdfb98fd18c1a6
rydenkot <45936536+rydenkot@users.noreply.github.com>
2021-06-29 15:29:53 +0300

    hello.java от 29.06

--- hello.java
+++ hello.java
@@ -1,17 +1,7 @@
 package com.max.idea;
 
 public class hello {
-    public static void main(String[] args)
-    {
-    int[] a =  {1, 2, 3, 5, 7};
-
-    int temp = a[0];
-        a[0] = a[4];
-        a[4] = temp;
-        System.out.print("Сумма первого и среднего элемента"+ " ");
-        System.out.println(a[0]+a[2]);
-
-
-}
-
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
 }
