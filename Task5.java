3e8abb45c573940a4707bdf7ac70b1149e392a16
rydenkot <45936536+rydenkot@users.noreply.github.com>
2021-07-10 01:04:13 +0300

    задание 5

---
+++ Task5.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+public class Task5 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите число 1:");
+        int x = sc.nextInt();
+        System.out.println("Введите число 2:");
+        int y = sc.nextInt();
+        System.out.println("Введите + - * или /");
+        char z = sc.next().charAt(0);
+        if (z == '+' ) {
+            System.out.println("x+y="+(x+y));
+        }
+        else if (z == '-' )
+        {
+            System.out.println("x-y="+(x-y));
+        }
+        else if (z == '*' )
+        {
+            System.out.println("x*y="+(x*y));
+        }
+        else if  (z == '/' )
+        {
+            System.out.println("x/y="+(x/y));
+        }
+
+        sc.close();
+    }
+}
