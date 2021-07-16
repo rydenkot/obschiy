003ce018be5fa419de0db2c6e6452d1529eeae79
rydenkot <45936536+rydenkot@users.noreply.github.com>
2021-07-16 11:04:14 +0300

    Задание 8

---
+++ Task_8.java
@@ -0,0 +1,21 @@
+
+    import java.util.Scanner;
+
+    public class Task_8 {
+
+        public static void main(String[] args) {
+            System.out.print("Введите любое целое положительное число: ");
+            Scanner sc = new Scanner(System.in);
+            int n = sc.nextInt();
+            int sum = 0;
+            for (int i=1; i<= n;  i++){
+
+                    if (i % 2 != 0)  {
+                        sum = sum + i;
+                    }
+            }
+            System.out.println (sum);
+            sc.close();
+        }
+    }
+
