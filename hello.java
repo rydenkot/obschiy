6fa6e72b1994c7cd9646f5301135ddb8b6d3670c
rydenkot <45936536+rydenkot@users.noreply.github.com>
2022-03-14 11:11:43 +0300

    Update hello.java

--- hello.java
+++ hello.java
@@ -1,3 +1,8 @@
+// Описание задания
+// Вывести на консоль текст “Hello World!”:
+// 1. Скачать и установить IntelliJ IDEA.
+// 2. Написать первую программу, которая выводит в консоль строку “Hello World!”.
+
 package com.max.idea;
 
 public class hello {
